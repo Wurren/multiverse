a5714ad909c62b994f4eb73326ecbda8557e4cc5
Warren Haskins <warrenhaskins@gmail.com>
2016-02-22 18:10:19 +0000

    fixed for blank email

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,6 +10,9 @@ class Admin::UsersController < ApplicationController
     end
 
     def create
+        if params[:user][:email].blank? 
+            return redirect_to new_admin_user_path, alert: "You Must Enter an Email Address!"
+        end
         @current_account.users.invite!(user_params)
         redirect_to admin_users_path, notice: "New User Invited!"
     end
